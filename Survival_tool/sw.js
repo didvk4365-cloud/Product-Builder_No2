4d83d06d5b9d0c4091c079db6078ef753568311d
Kanny_90 <didvk4365@gmail.com>
2026-05-07 04:32:30 +0900

    Push Survival Tool folder with fixed template literals to Product-Builder_No2

--- Survival_tool/sw.js
+++ Survival_tool/sw.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = 'survival-tool-v1';
-const urlsToCache = [
+const CACHE_NAME = 'survival-v1';
+const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './sw.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
