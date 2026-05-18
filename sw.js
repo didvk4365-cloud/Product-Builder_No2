865786e0be36a5e79b689248c51091db8a84ec54
didvk4365-cloud <didvk4365-cloud@users.noreply.github.com>
2026-05-19 03:33:45 +0900

    Initial commit of standalone Survival Tool

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'survival-tool-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // 대기하지 않고 즉시 설치
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim()); // 즉시 제어권 획득
+});
